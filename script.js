39dc967cb238f3984897a56ab69a64d5109a9097
Chris Melendez <chris.melendezg@gmail.com>
2021-03-04 13:23:40 -0600

    Add ui tags and response time

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const baseAPI = "https://api.stackexchange.com/2.2";
 const questions = `${baseAPI}/questions`;
-const filter = "!m)ASvzmwfr403f*F5dU1)8hbeB3Kgkc8rhKafuMzR-Es.)4fbDi5D6gX";
+const filter =
+  "!)PBpiQU6xq56c5frl-2DJSAs(FpuD)qYSIAko5H6ou)-KXU9SH(N4ZKrxuM-(MLcTy9yBm";
 
 const accordionId = "accordionQuestions";
 const errMsg = document.getElementById("error");
@@ -8,7 +9,7 @@ const errMsg = document.getElementById("error");
 function buildUrl(sort, tag) {
   const today = Math.floor(new Date().getTime() / 1000);
   const last = today - 7 * 24 * 60 * 60;
-  return `${questions}?fromdate=${last}&todate=${today}&order=desc&sort=${sort}&tagged=${tag}&site=stackoverflow&filter=${filter}`;
+  return `${questions}?pagesize=10&fromdate=${last}&todate=${today}&order=desc&sort=${sort}&tagged=${tag}&site=stackoverflow&filter=${filter}`;
 }
 
 function createAccordion(title, id, score, creation, rest) {
@@ -16,7 +17,11 @@ function createAccordion(title, id, score, creation, rest) {
   div.className = "accordion-item";
   const item = `<h2 class="accordion-header" id="heading${id}">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${id}">
-        <b>${title}</b> <br/> <i>Created: ${creation} / Score: ${score} </i>
+        <h4>
+          ${title}
+          <span class="badge bg-primary">Created: ${creation}</span>
+          <span class="badge bg-secondary">Score: ${score}</span>
+        </h4>
       </button>
     </h2>
     <div id="collapse${id}" class="accordion-collapse collapse" data-bs-parent="#${accordionId}">
@@ -32,7 +37,11 @@ function createCard(type, creation, score, body) {
   div.style.marginLeft = "10px";
   div.style.marginRight = "10px";
   div.innerHTML = `<div class="card-header">
-      ${type} Created: ${creation} Score: ${score}
+      <h4>
+        <span class="badge bg-warning text-dark">${type}</span>
+        <span class="badge bg-primary">Created: ${creation}</span>
+        <span class="badge bg-secondary">Score: ${score}</span>
+      </h4>
     </div>
     <div class="card-body">
       ${body}
@@ -45,7 +54,7 @@ function replies(type, items) {
   for (item of items) {
     const { creation_date, score } = item;
     const date = new Date(creation_date * 1000);
-    var element = createCard(type, date, score, item.body);
+    var element = createCard(type, date.toISOString(), score, item.body);
     if (type === "Answer" && item.comments)
       addChildren(element, replies("Comment", item.comments));
     children.push(element);
@@ -94,51 +103,53 @@ async function apiCall(url) {
   return json;
 }
 
-function extract(list, count) {
-  return list.slice(0, Math.min(list.length, count));
-}
-
 async function call() {
   const input = document.getElementById("tag");
   if (input.value) {
     var accordion = document.getElementById(accordionId);
     var button = document.getElementById("button");
     var spinner = document.getElementById("spinner");
+    var success = document.getElementById("success");
     accordion.innerHTML = "";
     spinner.style.display = "block";
     button.disabled = true;
-    errMsg.style.display = "none";
+    setAlert(errMsg, "none");
+    setAlert(success, "none");
     try {
+      const start = new Date();
       var url = buildUrl("creation", input.value);
-      const byCreation = await apiCall(url);
-      const created = extract(byCreation.items, 10);
+      const created = await apiCall(url);
 
       url = buildUrl("votes", input.value);
-      const byVotes = await apiCall(url);
-      const votes = extract(byVotes.items, 10);
+      const votes = await apiCall(url);
 
-      var questions = created.concat(votes);
+      var questions = created.items.concat(votes.items);
       if (questions && questions.length > 0) {
         questions.sort(function (first, second) {
-          return first.creation_date > second.creation_date;
+          return first.creation_date < second.creation_date;
         });
 
         elements(accordion, questions);
+        const now = new Date();
+        const respTime = `Successfully returned information in ${
+          (now - start) / 1000
+        } seconds`;
+        setAlert(success, "block", respTime);
       } else {
-        setError("Tag not found");
+        setAlert(errMsg, "block", "Tag not found");
       }
     } catch (err) {
       console.error(err);
-      setError("Something went wrong, please try again later");
+      setAlert(errMsg, "block", "Something went wrong, please try again later");
     }
     button.disabled = false;
     spinner.style.display = "none";
   } else {
-    setError("Please enter a tag");
+    setAlert(errMsg, "block", "Please enter a tag");
   }
 }
 
-function setError(msg) {
-  errMsg.innerHTML = msg;
-  errMsg.style.display = "block";
+function setAlert(elm, display, msg = "") {
+  elm.innerHTML = msg;
+  elm.style.display = display;
 }
